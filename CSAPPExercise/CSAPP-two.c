71ef1a1400a6638522d9a813e2e9837917ceefb3
pengzuoqing <pengzuoqing@163.com>
2020-01-07 14:26:15 +0800

    finish 2.95

--- CSAPPExercise/CSAPP-two.c
+++ CSAPPExercise/CSAPP-two.c
@@ -416,6 +416,23 @@ float_bits two_float_twice(float_bits f) {
     return (sign << 31) | (0xff << 23);
 }
 
+/// 2.95
+float_bits two_float_half(float_bits f) {
+    unsigned sign = f >> 31;
+    unsigned exp = f >> 23 & 0xff;
+    unsigned frac = f & 0x7fffff;
+    
+    if ((exp ^ 0xff) == 0 && frac != 0) {
+        return f;
+    } else if (exp == 0) {
+        return (sign << 31) | ((frac >> 1) + (frac & 3) == 3);
+    } else if (exp == 1) {
+        return (sign << 31) | ((exp - 1) << 23) | ((frac >> 1) | 0x400000);
+    }
+    
+    return (sign << 31) | ((exp - 1) << 23) | frac;
+}
+
 void callTwoFunction(void) {
     unsigned a = -1;
     a = a >> 1;
