e71f60727edae26146f500cd18dd0af8dac87b7a
pengzuoqing <pengzuoqing@163.com>
2020-03-14 18:14:20 +0800

    finish 3.62

--- CSAPPExercise/CSAPP-three.c
+++ CSAPPExercise/CSAPP-three.c
@@ -67,3 +67,36 @@ long three_create_alt(long *xp) {
     return *p;
 }
 
+typedef enum {MODE_A, MODE_B, MODE_C, MODE_D, MODE_E} mode_p;
+/// 3.62
+long three_switch3(long *p1, long *p2, mode_p action) {
+    long result = 0;
+    switch (action) {
+        case MODE_A:
+            result = *p2;
+            *p2 = *p1;
+            break;
+        case MODE_B:
+            result = *p1;
+            result += *p2;
+            *p1 = result;
+            break;
+        case MODE_C:
+            *p1 = 59;
+            result = *p2;
+            break;
+        case MODE_D:
+            result = *p2;
+            *p1 = result;
+            result = 27;
+            break;
+        case MODE_E:
+            result = 27;
+            break;
+        default:
+            result = 12;
+            break;
+    }
+    return result;
+}
+
