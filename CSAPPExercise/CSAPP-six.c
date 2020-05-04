3d2ac2d60a716e1fabef3b64d2663bb9b9316dc2
pengzuoqing <pengzuoqing@163.com>
2020-05-04 11:25:22 +0800

    finish 6.22

---
+++ CSAPPExercise/CSAPP-six.c
@@ -0,0 +1,16 @@
+//
+//  CSAPP-six.c
+//  CSAPPExercise
+//
+//  Created by 彭作青 on 2020/5/4.
+//  Copyright © 2020 彭作青. All rights reserved.
+//
+
+#include "CSAPP-six.h"
+
+// 6.22
+/*
+ C = x * r * 2 * (1-x) * r
+    x-x^2 最大，则x=0.5
+ */
+
---
+++ CSAPPExercise/CSAPP-six.h
@@ -0,0 +1,14 @@
+//
+//  CSAPP-six.h
+//  CSAPPExercise
+//
+//  Created by 彭作青 on 2020/5/4.
+//  Copyright © 2020 彭作青. All rights reserved.
+//
+
+#ifndef CSAPP_six_h
+#define CSAPP_six_h
+
+#include <stdio.h>
+
+#endif /* CSAPP_six_h */
